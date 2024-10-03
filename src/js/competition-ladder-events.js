b88866671348d6287e2af2a585c23ec1860ad46b
dayummson <educationalpurposesr@gmail.com>
2024-10-03 16:44:29 +0800

    feat: feature/frontend [competition ladder] added some click event for switching tabs

--- src/js/competition-ladder-events.js
+++ src/js/competition-ladder-events.js
@@ -1,2 +1,21 @@
+function switchTab(tab) {
+    const standingSection = document.getElementById('cl__standing');
+    const bracketSection = document.getElementById('cl__bracket');
 
+    const standingTab = document.getElementById('standing-tab-btn');
+    const bracketingTab = document.getElementById('bracketing-tab-btn');
 
+    if (tab === 'standing') {
+        standingSection.classList.remove('hide');
+        bracketSection.classList.add('hide');
+
+        standingTab.classList.add('tab-active');
+        bracketingTab.classList.remove('tab-active');
+    } else if (tab === 'bracketing') {
+        standingSection.classList.add('hide');
+        bracketSection.classList.remove('hide');
+
+        standingTab.classList.remove('tab-active');
+        bracketingTab.classList.add('tab-active');
+    }
+}
