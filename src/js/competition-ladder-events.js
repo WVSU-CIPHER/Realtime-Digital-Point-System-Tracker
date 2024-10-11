6143c48797ad02acc790c2cb245d4f9fea66b476
dayummson <educationalpurposer@gmail.com>
2024-10-11 12:38:10 +0800

    chore: refactor (competition-ladder-event.js), some changes in standing and bracketing section's classes

--- src/js/competition-ladder-events.js
+++ src/js/competition-ladder-events.js
@@ -1,41 +1,31 @@
+// Switch between tabs and handle showing/hiding sections
 function switchTab(tab) {
-    const standingSection = document.getElementById('cl__standing');
-    const bracketSection = document.getElementById('cl__bracket');
+    const sections = document.querySelectorAll(".container > section");
+    const tabs = document.querySelectorAll("nav > .tab");
 
-    const standingTab = document.getElementById('standing-tab-btn');
-    const bracketingTab = document.getElementById('bracketing-tab-btn');
+    // Hide the section that is not focused
+    sections.forEach(section =>
+        section.classList.toggle("hide", !section.classList.contains(tab))
+    );
 
-    if (tab === 'standing') {
-        standingSection.classList.remove('hide');
-        bracketSection.classList.add('hide');
+    // Set the tab button state when clicked
+    tabs.forEach(t =>
+        t.classList.toggle("tab-active", t.classList.contains(tab))
+    );
+};
 
-        standingTab.classList.add('tab-active');
-        bracketingTab.classList.remove('tab-active');
-    } else { // 'else focus on brackets'
-        standingSection.classList.add('hide');
-        bracketSection.classList.remove('hide');
-
-        standingTab.classList.remove('tab-active');
-        bracketingTab.classList.add('tab-active');
-    }
-}
-
+// Toggle the dropdown visibility and rotate the chevron
 function toggleDropdown() {
     const dropDownContent = document.getElementById("cl__drop-down-content");
     const dropDownBtn = document.getElementById("cl__drop-down-btn");
     const dropDownChev = document.querySelector(".cl__drop-down-chev-down");
 
-    if (dropDownContent.classList.contains("hide")) {
-        dropDownContent.classList.remove("hide");
-        dropDownContent.classList.add("show");
-        dropDownBtn.setAttribute("aria-expanded", "true");
-        dropDownChev.classList.add("rotate-feedback");
-    } else {
-        dropDownContent.classList.remove("show");
-        dropDownContent.classList.add("hide");
-        dropDownBtn.setAttribute("aria-expanded", "false");
-        dropDownChev.classList.remove("rotate-feedback");
-    }
-}
+    const isHidden = dropDownContent.classList.contains("hide");
+
+    dropDownContent.classList.toggle("hide", !isHidden);
+    dropDownContent.classList.toggle("show", isHidden);
+    dropDownBtn.setAttribute("aria-expanded", isHidden);
+    dropDownChev.classList.toggle("rotate-feedback", isHidden);
+};
 
 document.getElementById("cl__drop-down-btn").addEventListener("click", toggleDropdown);
